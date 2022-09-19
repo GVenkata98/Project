827b8fba1956f40aea0519b82436437b322a4129
nagendra-vanam <nagendravanam95@gmail.com>
2022-09-19 19:36:57 +0530

    resitration backend validation

--- server/Routes/register.js
+++ server/Routes/register.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const User = require("../models/registerSchema");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
+//user registration
 router.get("/", async (req, res) => {
   try {
     const data = await User.find();
@@ -17,14 +20,63 @@ router.get("/", async (req, res) => {
     });
   }
 });
-router.post("/", async (req, res) => {
-  try {
-    const data = await User.create(req.body);
-  } catch (e) {
-    res.json({
-      message: e.message,
-    });
+router.post(
+  "/",
+  body("email").isEmail(),
+  body("password").isLength({
+    min: 6,
+    max: 18,
+  }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { phonenumber, email, password } = req.body;
+      const existeduser = await User.findOne({ email });
+      const number = await User.findOne({ phonenumber });
+      // if (existeduser&&number) {
+      //   return res.status(400).json({
+      //     status: "registration failed",
+      //     message: "email already used",
+      //   });
+      // }
+      console.log(existeduser);
+      if (existeduser) {
+        return res.status(400).json({
+          status: "registration failed",
+          message: "email already used",
+        });
+      }
+      if (number) {
+        return res.status(400).json({
+          status: "registration failed",
+          message: "number already used",
+        });
+      }
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          return res.status(404).json({
+            status: "failed",
+          });
+        }
+        console.log(hash);
+        const users = await User.create({
+          ...req.body,
+          password: hash,
+        });
+        res.status(201).json({
+          satatus: "success",
+          users,
+        });
+      });
+    } catch (e) {
+      res.json({
+        message: e.message,
+      });
+    }
   }
-});
+);
 
 module.exports = router;
