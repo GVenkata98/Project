8636139cee14c1134f996f939d204cbde96fabd4
nagendra-vanam <nagendravanam95@gmail.com>
2022-09-23 18:00:12 +0530

    updated6

--- server/models/orderSchema.js
+++ server/models/orderSchema.js
@@ -2,70 +2,75 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const orderSchema = new mongoose.Schema(
   {
-    washtype:
-      // shirts: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: false },
-      //   ironing: { type: Boolean, defalut: false },
-      //   drying: { type: Boolean, defalut: false },
-      //   chemical_wash: { type: Boolean, defalut: false },
-      // },
-      // t_shirts: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      // trousers: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      // jeans: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      // boxers: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      // joggers: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      // others: {
-      //   quantity: { type: Number, default: 0 },
-      //   washing: { type: Boolean, defalut: 0 },
-      //   ironing: { type: Boolean, defalut: 0 },
-      //   drying: { type: Boolean, defalut: 0 },
-      //   chemical_wash: { type: Boolean, defalut: 0 },
-      // },
-      Array,
-    totalquantity: String,
-    totalprice: String,
+    shirts: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: false },
+      ironing: { type: Boolean, defalut: false },
+      drying: { type: Boolean, defalut: false },
+      chemical_wash: { type: Boolean, defalut: false },
+    },
+    t_shirts: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+    trousers: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+    jeans: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+    boxers: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+    joggers: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+    others: {
+      quantity: { type: Number, default: 0 },
+      washing: { type: Boolean, defalut: 0 },
+      ironing: { type: Boolean, defalut: 0 },
+      drying: { type: Boolean, defalut: 0 },
+      chemical_wash: { type: Boolean, defalut: 0 },
+    },
+
+    totalquantity: { String, required: true },
+
     orderdate: {
       type: String,
+      required: true,
     },
-    subtotalcost: Number,
-    totalcost: Number,
-  
-    user: { type: Schema.Types.ObjectId, ref: "registerschema" },
+    subtotalcost: { type: String, required: true },
+    totalcost: { type: String, required: true },
+
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "registerschema",
+      required: true,
+    },
     storelocation: {
       type: String,
+      required: true,
     },
-    phonenumber: String,
+    phonenumber: { String, required: true },
   },
 
   { timestamps: true }
